c29be8e9a02e7321fdf7aecb9b00d1b66094296d
johnpoint <me@lvcshu.com>
2023-12-28 17:59:13 +0800

    pref: BilibiliNews

--- bot/scheduled/arknights_news.go
+++ bot/scheduled/arknights_news.go
@@ -7,43 +7,47 @@ import (
 )
 
 func BilibiliNews() func() {
-	bilibiliNews := func() {
+	return func() {
 		text, pics := utils.ParseBilibiliDynamic()
-		if text != "" {
-			groups := utils.GetJoinedGroups()
-			if pics == nil {
-				for _, group := range groups {
-					groupNumber, _ := strconv.ParseInt(group, 10, 64)
-					sendMessage := tgbotapi.NewMessage(groupNumber, text)
-					utils.SendMessage(sendMessage)
-				}
-			} else if len(pics) == 1 {
-				for _, group := range groups {
-					groupNumber, _ := strconv.ParseInt(group, 10, 64)
-					sendPhoto := tgbotapi.NewPhoto(groupNumber, tgbotapi.FileURL(pics[0]))
-					sendPhoto.Caption = text
-					utils.SendPhoto(sendPhoto)
-				}
-			} else {
-				for _, group := range groups {
-					groupNumber, _ := strconv.ParseInt(group, 10, 64)
-					var mediaGroup tgbotapi.MediaGroupConfig
-					var media []interface{}
-					mediaGroup.ChatID = groupNumber
-					for i, pic := range pics {
-						var inputPhoto tgbotapi.InputMediaPhoto
-						inputPhoto.Media = tgbotapi.FileURL(pic)
-						inputPhoto.Type = "photo"
-						if i == 0 {
-							inputPhoto.Caption = text
-						}
-						media = append(media, inputPhoto)
-					}
-					mediaGroup.Media = media
-					utils.SendMediaGroup(mediaGroup)
-				}
+		if len(text) == 0 {
+			return
+		}
+		groups := utils.GetJoinedGroups()
+		if pics == nil {
+			for _, group := range groups {
+				groupNumber, _ := strconv.ParseInt(group, 10, 64)
+				sendMessage := tgbotapi.NewMessage(groupNumber, text)
+				utils.SendMessage(sendMessage)
 			}
+			return
+		}
+
+		if len(pics) == 1 {
+			for _, group := range groups {
+				groupNumber, _ := strconv.ParseInt(group, 10, 64)
+				sendPhoto := tgbotapi.NewPhoto(groupNumber, tgbotapi.FileURL(pics[0]))
+				sendPhoto.Caption = text
+				utils.SendPhoto(sendPhoto)
+			}
+			return
+		}
+
+		for _, group := range groups {
+			groupNumber, _ := strconv.ParseInt(group, 10, 64)
+			var mediaGroup tgbotapi.MediaGroupConfig
+			var media []interface{}
+			mediaGroup.ChatID = groupNumber
+			for i, pic := range pics {
+				var inputPhoto tgbotapi.InputMediaPhoto
+				inputPhoto.Media = tgbotapi.FileURL(pic)
+				inputPhoto.Type = "photo"
+				if i == 0 {
+					inputPhoto.Caption = text
+				}
+				media = append(media, inputPhoto)
+			}
+			mediaGroup.Media = media
+			utils.SendMediaGroup(mediaGroup)
 		}
 	}
-	return bilibiliNews
 }
