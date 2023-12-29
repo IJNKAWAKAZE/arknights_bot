3396f9619e3fcd14ab76ee9756c1c1ab89ee3783
johnpoint <me@lvcshu.com>
2023-12-29 10:48:00 +0800

    pref: 简化代码

--- bot/handle/new_member_handle.go
+++ bot/handle/new_member_handle.go
@@ -2,30 +2,29 @@ package handle
 
 import (
 	"arknights_bot/bot/utils"
+	"fmt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	"strconv"
 )
 
 func NewMemberHandle(update tgbotapi.Update) (bool, error) {
 	message := update.Message
-	chatId := message.Chat.ID
-	userId := message.From.ID
-	messageId := message.MessageID
-	delMsg := tgbotapi.NewDeleteMessage(chatId, messageId)
+	delMsg := tgbotapi.NewDeleteMessage(message.Chat.ID, message.MessageID)
 	utils.DeleteMessage(delMsg)
 	for _, member := range message.NewChatMembers {
-		memberId := member.ID
-		if memberId == userId {
+		if member.ID == message.From.ID { // 自己加入群组
 			go Verify(message)
-		} else {
-			utils.SaveInvite(message, member)
-			name := utils.GetFullName(message.From)
-			newName := utils.GetNewMemberName(member)
-			sendMessage := tgbotapi.NewMessage(chatId, "<a href=\"tg://user?id="+strconv.FormatInt(userId, 10)+"\">"+name+"</a>邀请了<a href=\"tg://user?id="+strconv.FormatInt(memberId, 10)+"\">"+newName+"</a>加入群组。")
-			sendMessage.ParseMode = tgbotapi.ModeHTML
-			msg, _ := utils.SendMessage(sendMessage)
-			utils.AddDelQueue(msg.Chat.ID, msg.MessageID, 2)
+			continue
 		}
+		// 邀请加入群组，无需进行验证
+		utils.SaveInvite(message, member)
+		name := utils.GetFullName(message.From)
+		newName := utils.GetNewMemberName(member)
+		sendMessage := tgbotapi.NewMessage(message.Chat.ID,
+			fmt.Sprintf("[%s](tg://user?id=%d)邀请了[%s](tg://user?id=%d)加入群组。",
+				name, message.From.ID, newName, member.ID))
+		sendMessage.ParseMode = tgbotapi.ModeMarkdownV2
+		msg, _ := utils.SendMessage(sendMessage)
+		utils.AddDelQueue(msg.Chat.ID, msg.MessageID, 2)
 	}
 	return true, nil
 }
