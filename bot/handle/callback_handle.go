005581ffa55e9044fd28fba4553df100c861679a
johnpoint <me@lvcshu.com>
2023-12-29 11:25:48 +0800

    pref: 拆分

--- bot/handle/callback_handle.go
+++ bot/handle/callback_handle.go
@@ -2,82 +2,92 @@ package handle
 
 import (
 	"arknights_bot/bot/utils"
+	"fmt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"strconv"
 	"strings"
 )
 
 func CallBackData(callBack tgbotapi.Update) (bool, error) {
-	if callBack.CallbackQuery != nil {
-		callbackQuery := callBack.CallbackQuery
-		data := callBack.CallbackData()
-		d := strings.Split(data, ",")
-		userId, _ := strconv.ParseInt(d[0], 10, 64)
-		chatId := callbackQuery.Message.Chat.ID
-		name := utils.GetFullName(callbackQuery.From)
-		if d[1] == "PASS" || d[1] == "BAN" {
-			chatConfigWithUser := tgbotapi.ChatConfigWithUser{
+	callbackQuery := callBack.CallbackQuery
+	data := callBack.CallbackData()
+	d := strings.Split(data, ",")
+
+	if len(d) < 3 {
+		return true, nil
+	}
+
+	userId, _ := strconv.ParseInt(d[0], 10, 64)
+	chatId := callbackQuery.Message.Chat.ID
+	name := utils.GetFullName(callbackQuery.From)
+
+	if d[1] == "PASS" || d[1] == "BAN" {
+		getChatMemberConfig := tgbotapi.GetChatMemberConfig{
+			ChatConfigWithUser: tgbotapi.ChatConfigWithUser{
 				ChatID: chatId,
 				UserID: callbackQuery.From.ID,
-			}
-			getChatMemberConfig := tgbotapi.GetChatMemberConfig{
-				ChatConfigWithUser: chatConfigWithUser,
-			}
-			memberInfo, _ := utils.GetChatMemberInfo(getChatMemberConfig)
-			role := memberInfo.Status
-			if role == "creator" || role == "administrator" {
-				if d[1] == "PASS" {
-					pass(chatId, userId, callbackQuery, "管理员通过了<a href=\"tg://user?id="+strconv.FormatInt(userId, 10)+"\">"+d[2]+"</a>的验证")
-				} else if d[1] == "BAN" {
-					chatMember := tgbotapi.ChatMemberConfig{ChatID: chatId, UserID: userId}
-					ban(chatId, userId, callbackQuery, chatMember, "管理员封禁了<a href=\"tg://user?id="+strconv.FormatInt(userId, 10)+"\">"+d[2]+"</a>")
-				}
-			} else {
-				answer := tgbotapi.NewCallbackWithAlert(callbackQuery.ID, "无使用权限！")
-				utils.SendCallbackAnswer(answer)
-			}
+			},
+		}
+		memberInfo, _ := utils.GetChatMemberInfo(getChatMemberConfig)
+
+		if memberInfo.Status != "creator" && memberInfo.Status != "administrator" {
+			answer := tgbotapi.NewCallbackWithAlert(callbackQuery.ID, "无使用权限！")
+			utils.SendCallbackAnswer(answer)
 			return true, nil
 		}
-		if d[0] != strconv.FormatInt(callbackQuery.From.ID, 10) {
-			answer := tgbotapi.NewCallbackWithAlert(callbackQuery.ID, "这不是你的验证！")
-			utils.SendCallbackAnswer(answer)
-		} else {
-			if d[1] == d[2] {
-				answer := tgbotapi.NewCallbackWithAlert(callbackQuery.ID, "验证通过！")
-				utils.SendCallbackAnswer(answer)
-				pass(chatId, userId, callbackQuery, "<a href=\"tg://user?id="+strconv.FormatInt(userId, 10)+"\">"+name+"</a>已完成验证")
-			} else {
-				answer := tgbotapi.NewCallbackWithAlert(callbackQuery.ID, "验证未通过！")
-				utils.SendCallbackAnswer(answer)
-				chatMember := tgbotapi.ChatMemberConfig{ChatID: chatId, UserID: userId}
-				ban(chatId, userId, callbackQuery, chatMember, "<a href=\"tg://user?id="+strconv.FormatInt(userId, 10)+"\">"+name+"</a>未通过验证，已被踢出。")
-				go unban(chatMember)
-			}
+
+		if d[1] == "PASS" {
+			pass(chatId, userId, callbackQuery, "管理员通过了<a href=\"tg://user?id="+strconv.FormatInt(userId, 10)+"\">"+d[2]+"</a>的验证")
 		}
+
+		if d[1] == "BAN" {
+			chatMember := tgbotapi.ChatMemberConfig{ChatID: chatId, UserID: userId}
+			ban(chatId, userId, callbackQuery, chatMember, "管理员封禁了<a href=\"tg://user?id="+strconv.FormatInt(userId, 10)+"\">"+d[2]+"</a>")
+		}
+
+		return true, nil
 	}
+
+	if userId != callbackQuery.From.ID {
+		answer := tgbotapi.NewCallbackWithAlert(callbackQuery.ID, "这不是你的验证！")
+		utils.SendCallbackAnswer(answer)
+		return true, nil
+	}
+
+	if d[1] != d[2] {
+		answer := tgbotapi.NewCallbackWithAlert(callbackQuery.ID, "验证未通过！")
+		utils.SendCallbackAnswer(answer)
+		chatMember := tgbotapi.ChatMemberConfig{ChatID: chatId, UserID: userId}
+		ban(chatId, userId, callbackQuery, chatMember, "<a href=\"tg://user?id="+strconv.FormatInt(userId, 10)+"\">"+name+"</a>未通过验证，已被踢出。")
+		go unban(chatMember)
+		return true, nil
+	}
+
+	answer := tgbotapi.NewCallbackWithAlert(callbackQuery.ID, "验证通过！")
+	utils.SendCallbackAnswer(answer)
+	pass(chatId, userId, callbackQuery, "<a href=\"tg://user?id="+strconv.FormatInt(userId, 10)+"\">"+name+"</a>已完成验证")
+
 	return true, nil
 }
 
 func pass(chatId int64, userId int64, callbackQuery *tgbotapi.CallbackQuery, text string) string {
-	chatPermissions := tgbotapi.ChatPermissions{
-		CanSendMessages:       true,
-		CanSendMediaMessages:  true,
-		CanSendPolls:          true,
-		CanSendOtherMessages:  true,
-		CanAddWebPagePreviews: true,
-		CanInviteUsers:        true,
-		CanChangeInfo:         true,
-		CanPinMessages:        true,
-	}
-	restrictChatMemberConfig := tgbotapi.RestrictChatMemberConfig{
-		Permissions: &chatPermissions,
-	}
-	restrictChatMemberConfig.ChatID = chatId
-	restrictChatMemberConfig.UserID = userId
-	utils.SetMemberPermissions(restrictChatMemberConfig)
-	cid := strconv.FormatInt(chatId, 10)
-	uid := strconv.FormatInt(userId, 10)
-	val := "verify" + cid + uid
+	utils.SetMemberPermissions(tgbotapi.RestrictChatMemberConfig{
+		Permissions: &tgbotapi.ChatPermissions{
+			CanSendMessages:       true,
+			CanSendMediaMessages:  true,
+			CanSendPolls:          true,
+			CanSendOtherMessages:  true,
+			CanAddWebPagePreviews: true,
+			CanInviteUsers:        true,
+			CanChangeInfo:         true,
+			CanPinMessages:        true,
+		},
+		ChatMemberConfig: tgbotapi.ChatMemberConfig{
+			ChatID: chatId,
+			UserID: userId,
+		},
+	})
+	val := fmt.Sprintf("verify%d%d", chatId, userId)
 	utils.RedisDelSetItem("verify", val)
 	delMsg := tgbotapi.NewDeleteMessage(chatId, callbackQuery.Message.MessageID)
 	utils.DeleteMessage(delMsg)
