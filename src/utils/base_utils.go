5d524cc48ebd94f84687ee64ff93b558eff134ab
johnpoint <me@lvcshu.com>
2024-01-02 11:19:06 +0800

    refactor: 代码归类

--- src/utils/base_utils.go
+++ src/utils/base_utils.go
@@ -28,6 +28,20 @@ func GetFullName(user *tgbotapi.User) string {
 	return buffer.String()
 }
 
+type GroupInvite struct {
+	Id           string    `json:"id" gorm:"primaryKey"`
+	GroupName    string    `json:"groupName"`
+	GroupNumber  string    `json:"groupNumber"`
+	UserName     string    `json:"userName"`
+	UserNumber   string    `json:"userNumber"`
+	MemberName   string    `json:"memberName"`
+	MemberNumber string    `json:"memberNumber"`
+	CreateTime   time.Time `json:"createTime" gorm:"autoCreateTime"`
+	UpdateTime   time.Time `json:"updateTime" gorm:"autoUpdateTime"`
+	Remark       string    `json:"remark"`
+	Deleted      int64     `json:"deleted"`
+}
+
 // SaveInvite 保存邀请记录
 func SaveInvite(message *tgbotapi.Message, member *tgbotapi.User) {
 	id, _ := gonanoid.New(32)
