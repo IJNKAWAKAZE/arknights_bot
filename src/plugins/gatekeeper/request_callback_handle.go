1bb88b1661acfc3d8221938efef3176aed6e9a28
Tenko <IJNKAWAKAZE@protonmail.com>
2025-07-09 10:06:14 +0800

    申请模式的入群提醒

--- src/plugins/gatekeeper/request_callback_handle.go
+++ src/plugins/gatekeeper/request_callback_handle.go
@@ -2,6 +2,9 @@ package gatekeeper
 
 import (
 	bot "arknights_bot/config"
+	"arknights_bot/plugins/messagecleaner"
+	"arknights_bot/utils"
+	"fmt"
 	tgbotapi "github.com/ijnkawakaze/telegram-bot-api"
 	"strconv"
 	"strings"
@@ -28,6 +31,24 @@ func RequestCallBackData(callBack tgbotapi.Update) error {
 			callbackQuery.Answer(true, "验证通过！")
 			approveChatJoinRequest := tgbotapi.ApproveChatJoinRequestConfig{ChatConfig: tgbotapi.ChatConfig{ChatID: chatId}, UserID: userId}
 			bot.Arknights.Request(approveChatJoinRequest)
+			// 新人入群提醒
+			var joined utils.GroupJoined
+			utils.GetJoinedByChatId(chatId).Scan(&joined)
+			var welcome string
+			if joined.Welcome != "" {
+				welcome = "，" + joined.Welcome
+			}
+			text := fmt.Sprintf("欢迎[%s](tg://user?id=%d)%s\n", tgbotapi.EscapeText(tgbotapi.ModeMarkdownV2, callbackQuery.From.FullName()), callbackQuery.From.ID, welcome)
+			if joined.Reg != -1 {
+				text += fmt.Sprintf("建议阅读群公约：[点击阅读](https://t.me/%s/%d)", callbackQuery.Message.Chat.UserName, joined.Reg)
+			}
+			sendMessage := tgbotapi.NewMessage(chatId, text)
+			sendMessage.ParseMode = tgbotapi.ModeMarkdownV2
+			msg, err := bot.Arknights.Send(sendMessage)
+			if err != nil {
+				return err
+			}
+			messagecleaner.AddDelQueue(chatId, msg.MessageID, 3600)
 		}
 		callbackQuery.Delete()
 	}
