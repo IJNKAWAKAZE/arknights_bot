90b76cc9b825320800e5ff1f3b4a1dcb116db4c5
Tenko <IJNKAWAKAZE@protonmail.com>
2025-07-30 11:02:13 +0800

    新版本的敌人查询

--- src/plugins/enemy/parse_enemy.go
+++ src/plugins/enemy/parse_enemy.go
@@ -17,48 +17,37 @@ type Enemy struct {
 	EnemyLevel string        `json:"enemyLevel"` // 地位级别
 	AttackType string        `json:"attackType"` // 攻击方式
 	Motion     string        `json:"motion"`     // 行动方式
-	Endure     string        `json:"endure"`     // 耐久
-	Attack     string        `json:"attack"`     // 攻击力
-	Defence    string        `json:"defence"`    // 防御力
-	Resistance string        `json:"resistance"` // 法抗
-	MoveSpeed  string        `json:"moveSpeed"`  // 移动速度
-	ATKSpeed   string        `json:"ATKSpeed"`   // 攻击速度
-	ElementRes string        `json:"elementRes"` // 元素抵抗
-	DamageRes  string        `json:"damageRes"`  // 损伤抵抗
 	Ability    template.HTML `json:"ability"`    // 能力
 	Levels     []Level       `json:"level"`      // 级别信息
 }
 
 type Level struct {
+	Desc       string        `json:"desc"`       // 描述
+	AttackType string        `json:"attackType"` // 攻击方式
+	Motion     string        `json:"motion"`     // 行动方式
+	HpRecovery string        `json:"hpRecovery"` // 生命恢复速度
 	HP         string        `json:"hp"`         // 生命值
 	ATK        string        `json:"atk"`        // 攻击
 	DEF        string        `json:"def"`        // 防御
 	Res        string        `json:"res"`        // 法抗
-	Interval   string        `json:"interval"`   // 攻击间隔
 	ATKRadius  string        `json:"ATKRadius"`  // 攻击半径
-	Point      string        `json:"point"`      // 进点损失
-	MoveSpeed  string        `json:"moveSpeed"`  // 移动速度
 	Weight     string        `json:"weight"`     // 重量
-	ElementRes string        `json:"elementRes"` // 元素抵抗
+	MoveSpeed  string        `json:"moveSpeed"`  // 移动速度
+	Interval   string        `json:"interval"`   // 攻击间隔
 	DamageRes  string        `json:"damageRes"`  // 损伤抵抗
-	HpRecovery string        `json:"hpRecovery"` // 生命恢复速度
-	Silence    string        `json:"silence"`    // 沉默免疫
-	Dizziness  string        `json:"dizziness"`  // 眩晕免疫
-	Sleep      string        `json:"sleep"`      // 睡眠免疫
-	Freeze     string        `json:"freeze"`     // 冻结免疫
-	Float      string        `json:"float"`      // 浮空免疫
-	Tremble    string        `json:"tremble"`    // 战栗免疫
-	Fear       string        `json:"fear"`       // 恐惧抗性
+	ElementRes string        `json:"elementRes"` // 元素抵抗
+	Ridicule   string        `json:"ridicule"`   // 基础嘲讽等级
+	Point      string        `json:"point"`      // 进点损失
+	Abnormal   string        `json:"abnormal"`   // 异常抗性
 	Skills     []Skill       `json:"skills"`     // 技能
 	Talent     template.HTML `json:"talent"`     // 天赋&能力
 }
 
 type Skill struct {
-	Name      string        `json:"name"`      // 名称
-	SpInit    string        `json:"spInit"`    // 初始sp
-	SpCost    string        `json:"spCost"`    // 所需sp
-	SkillType template.HTML `json:"skillType"` // 技能类型
-	Desc      template.HTML `json:"desc"`      // 描述
+	Name   string        `json:"name"`   // 名称
+	SpInit template.HTML `json:"spInit"` // 初始sp
+	SpCost template.HTML `json:"spCost"` // 所需sp
+	Desc   template.HTML `json:"desc"`   // 描述
 }
 
 func ParseEnemy(name string) Enemy {
@@ -77,17 +66,7 @@ func ParseEnemy(name string) Enemy {
 	enemy.EnemyLevel = strings.ReplaceAll(td3.Eq(1).Text(), "\n", "")
 	enemy.AttackType = strings.ReplaceAll(td3.Eq(2).Text(), "\n", "")
 	enemy.Motion = strings.ReplaceAll(td3.Eq(3).Text(), "\n", "")
-	td5 := trs.Eq(5).Find("td")
-	enemy.Endure = strings.ReplaceAll(td5.Eq(0).Text(), "\n", "")
-	enemy.Attack = strings.ReplaceAll(td5.Eq(1).Text(), "\n", "")
-	enemy.Defence = strings.ReplaceAll(td5.Eq(2).Text(), "\n", "")
-	enemy.Resistance = strings.ReplaceAll(td5.Eq(3).Text(), "\n", "")
-	td7 := trs.Eq(7).Find("td")
-	enemy.MoveSpeed = strings.ReplaceAll(td7.Eq(0).Text(), "\n", "")
-	enemy.ATKSpeed = strings.ReplaceAll(td7.Eq(1).Text(), "\n", "")
-	enemy.ElementRes = strings.ReplaceAll(td7.Eq(2).Text(), "\n", "")
-	enemy.DamageRes = strings.ReplaceAll(td7.Eq(3).Text(), "\n", "")
-	ability, _ := trs.Eq(9).Html()
+	ability, _ := trs.Eq(5).Html()
 	enemy.Ability = template.HTML(ability)
 
 	// 级别详情
@@ -99,45 +78,46 @@ func ParseEnemy(name string) Enemy {
 				if j == 0 {
 					trs := selection.Find("tr")
 					td3 := trs.Eq(3).Find("td")
-					level.HP = strings.ReplaceAll(td3.Eq(0).Text(), "\n", "")
-					level.ATK = strings.ReplaceAll(td3.Eq(1).Text(), "\n", "")
-					level.DEF = strings.ReplaceAll(td3.Eq(2).Text(), "\n", "")
-					level.Res = strings.ReplaceAll(td3.Eq(3).Text(), "\n", "")
-					level.Interval = strings.ReplaceAll(td3.Eq(4).Text(), "\n", "")
-					level.ATKRadius = strings.ReplaceAll(td3.Eq(5).Text(), "\n", "")
+					level.Desc = strings.ReplaceAll(td3.Eq(0).Text(), "\n", "")
 
-					td5 := trs.Eq(5).Find("td")
-					level.Point = strings.ReplaceAll(td5.Eq(0).Text(), "\n", "")
-					level.MoveSpeed = strings.ReplaceAll(td5.Eq(1).Text(), "\n", "")
-					level.Weight = strings.ReplaceAll(td5.Eq(2).Text(), "\n", "")
-					level.ElementRes = strings.ReplaceAll(td5.Eq(3).Text(), "\n", "")
-					level.DamageRes = strings.ReplaceAll(td5.Eq(4).Text(), "\n", "")
+					td5 := trs.Eq(6).Find("td")
+					level.AttackType = strings.ReplaceAll(td5.Eq(1).Text(), "\n", "")
+					level.Motion = strings.ReplaceAll(td5.Eq(3).Text(), "\n", "")
 					level.HpRecovery = strings.ReplaceAll(td5.Eq(5).Text(), "\n", "")
+
+					td6 := trs.Eq(6).Find("td")
+					level.HP = strings.ReplaceAll(td6.Eq(0).Text(), "\n", "")
+					level.ATK = strings.ReplaceAll(td6.Eq(1).Text(), "\n", "")
+					level.DEF = strings.ReplaceAll(td6.Eq(2).Text(), "\n", "")
+					level.Res = strings.ReplaceAll(td6.Eq(3).Text(), "\n", "")
+					level.ATKRadius = strings.ReplaceAll(td6.Eq(4).Text(), "\n", "")
+					level.Weight = strings.ReplaceAll(td6.Eq(5).Text(), "\n", "")
+
+					td8 := trs.Eq(8).Find("td")
+					level.MoveSpeed = strings.ReplaceAll(td8.Eq(0).Text(), "\n", "")
+					level.Interval = strings.ReplaceAll(td8.Eq(1).Text(), "\n", "")
+					level.DamageRes = strings.ReplaceAll(td8.Eq(2).Text(), "\n", "")
+					level.ElementRes = strings.ReplaceAll(td8.Eq(3).Text(), "\n", "")
+					level.Ridicule = strings.ReplaceAll(td8.Eq(4).Text(), "\n", "")
+					level.Point = strings.ReplaceAll(td8.Eq(5).Text(), "\n", "")
+
+					td9 := trs.Eq(9).Find("td")
+					level.Abnormal = strings.ReplaceAll(td9.Eq(0).Text(), "\n", "")
 				}
 				if j == 1 {
-					tds := selection.Find("tr").Eq(2).Find("td")
-					level.Silence = strings.ReplaceAll(tds.Eq(0).Text(), "\n", "")
-					level.Dizziness = strings.ReplaceAll(tds.Eq(1).Text(), "\n", "")
-					level.Sleep = strings.ReplaceAll(tds.Eq(2).Text(), "\n", "")
-					level.Freeze = strings.ReplaceAll(tds.Eq(3).Text(), "\n", "")
-					level.Float = strings.ReplaceAll(tds.Eq(4).Text(), "\n", "")
-					level.Tremble = strings.ReplaceAll(tds.Eq(5).Text(), "\n", "")
-					level.Fear = strings.ReplaceAll(tds.Eq(6).Text(), "\n", "")
-				}
-				if j == 2 {
 					var skills []Skill
 					selection.Find("tr").Each(func(k int, selection *goquery.Selection) {
 						if !selection.Children().First().Is("th") {
 							tds := selection.Find("td")
 							// 技能
-							if len(tds.Nodes) == 5 {
+							if len(tds.Nodes) == 4 {
 								var skill Skill
 								skill.Name = strings.ReplaceAll(tds.Eq(0).Text(), "\n", "")
-								skill.SpInit = strings.ReplaceAll(tds.Eq(1).Text(), "\n", "")
-								skill.SpCost = strings.ReplaceAll(tds.Eq(2).Text(), "\n", "")
-								skillType, _ := tds.Eq(3).Html()
-								skill.SkillType = template.HTML(skillType)
-								desc, _ := tds.Eq(4).Html()
+								spInit, _ := tds.Eq(1).Html()
+								skill.SpInit = template.HTML(spInit)
+								spCost, _ := tds.Eq(2).Html()
+								skill.SpCost = template.HTML(spCost)
+								desc, _ := tds.Eq(3).Html()
 								skill.Desc = template.HTML(desc)
 								skills = append(skills, skill)
 							}
@@ -159,45 +139,46 @@ func ParseEnemy(name string) Enemy {
 				if j == 0 {
 					trs := selection.Find("tr")
 					td3 := trs.Eq(3).Find("td")
-					level.HP = strings.ReplaceAll(td3.Eq(0).Text(), "\n", "")
-					level.ATK = strings.ReplaceAll(td3.Eq(1).Text(), "\n", "")
-					level.DEF = strings.ReplaceAll(td3.Eq(2).Text(), "\n", "")
-					level.Res = strings.ReplaceAll(td3.Eq(3).Text(), "\n", "")
-					level.Interval = strings.ReplaceAll(td3.Eq(4).Text(), "\n", "")
-					level.ATKRadius = strings.ReplaceAll(td3.Eq(5).Text(), "\n", "")
+					level.Desc = strings.ReplaceAll(td3.Eq(0).Text(), "\n", "")
 
-					td5 := trs.Eq(5).Find("td")
-					level.Point = strings.ReplaceAll(td5.Eq(0).Text(), "\n", "")
-					level.MoveSpeed = strings.ReplaceAll(td5.Eq(1).Text(), "\n", "")
-					level.Weight = strings.ReplaceAll(td5.Eq(2).Text(), "\n", "")
-					level.ElementRes = strings.ReplaceAll(td5.Eq(3).Text(), "\n", "")
-					level.DamageRes = strings.ReplaceAll(td5.Eq(4).Text(), "\n", "")
+					td5 := trs.Eq(6).Find("td")
+					level.AttackType = strings.ReplaceAll(td5.Eq(1).Text(), "\n", "")
+					level.Motion = strings.ReplaceAll(td5.Eq(3).Text(), "\n", "")
 					level.HpRecovery = strings.ReplaceAll(td5.Eq(5).Text(), "\n", "")
+
+					td6 := trs.Eq(6).Find("td")
+					level.HP = strings.ReplaceAll(td6.Eq(0).Text(), "\n", "")
+					level.ATK = strings.ReplaceAll(td6.Eq(1).Text(), "\n", "")
+					level.DEF = strings.ReplaceAll(td6.Eq(2).Text(), "\n", "")
+					level.Res = strings.ReplaceAll(td6.Eq(3).Text(), "\n", "")
+					level.ATKRadius = strings.ReplaceAll(td6.Eq(4).Text(), "\n", "")
+					level.Weight = strings.ReplaceAll(td6.Eq(5).Text(), "\n", "")
+
+					td8 := trs.Eq(8).Find("td")
+					level.MoveSpeed = strings.ReplaceAll(td8.Eq(0).Text(), "\n", "")
+					level.Interval = strings.ReplaceAll(td8.Eq(1).Text(), "\n", "")
+					level.DamageRes = strings.ReplaceAll(td8.Eq(2).Text(), "\n", "")
+					level.ElementRes = strings.ReplaceAll(td8.Eq(3).Text(), "\n", "")
+					level.Ridicule = strings.ReplaceAll(td8.Eq(4).Text(), "\n", "")
+					level.Point = strings.ReplaceAll(td8.Eq(5).Text(), "\n", "")
+
+					td9 := trs.Eq(9).Find("td")
+					level.Abnormal = strings.ReplaceAll(td9.Eq(0).Text(), "\n", "")
 				}
 				if j == 1 {
-					tds := selection.Find("tr").Eq(2).Find("td")
-					level.Silence = strings.ReplaceAll(tds.Eq(0).Text(), "\n", "")
-					level.Dizziness = strings.ReplaceAll(tds.Eq(1).Text(), "\n", "")
-					level.Sleep = strings.ReplaceAll(tds.Eq(2).Text(), "\n", "")
-					level.Freeze = strings.ReplaceAll(tds.Eq(3).Text(), "\n", "")
-					level.Float = strings.ReplaceAll(tds.Eq(4).Text(), "\n", "")
-					level.Tremble = strings.ReplaceAll(tds.Eq(5).Text(), "\n", "")
-					level.Fear = strings.ReplaceAll(tds.Eq(6).Text(), "\n", "")
-				}
-				if j == 2 {
 					var skills []Skill
 					selection.Find("tr").Each(func(k int, selection *goquery.Selection) {
 						if !selection.Children().First().Is("th") {
 							tds := selection.Find("td")
 							// 技能
-							if len(tds.Nodes) == 5 {
+							if len(tds.Nodes) == 4 {
 								var skill Skill
 								skill.Name = strings.ReplaceAll(tds.Eq(0).Text(), "\n", "")
-								skill.SpInit = strings.ReplaceAll(tds.Eq(1).Text(), "\n", "")
-								skill.SpCost = strings.ReplaceAll(tds.Eq(2).Text(), "\n", "")
-								skillType, _ := tds.Eq(3).Html()
-								skill.SkillType = template.HTML(skillType)
-								desc, _ := tds.Eq(4).Html()
+								spInit, _ := tds.Eq(1).Html()
+								skill.SpInit = template.HTML(spInit)
+								spCost, _ := tds.Eq(2).Html()
+								skill.SpCost = template.HTML(spCost)
+								desc, _ := tds.Eq(3).Html()
 								skill.Desc = template.HTML(desc)
 								skills = append(skills, skill)
 							}
@@ -219,45 +200,46 @@ func ParseEnemy(name string) Enemy {
 				if j == 0 {
 					trs := selection.Find("tr")
 					td3 := trs.Eq(3).Find("td")
-					level.HP = strings.ReplaceAll(td3.Eq(0).Text(), "\n", "")
-					level.ATK = strings.ReplaceAll(td3.Eq(1).Text(), "\n", "")
-					level.DEF = strings.ReplaceAll(td3.Eq(2).Text(), "\n", "")
-					level.Res = strings.ReplaceAll(td3.Eq(3).Text(), "\n", "")
-					level.Interval = strings.ReplaceAll(td3.Eq(4).Text(), "\n", "")
-					level.ATKRadius = strings.ReplaceAll(td3.Eq(5).Text(), "\n", "")
+					level.Desc = strings.ReplaceAll(td3.Eq(0).Text(), "\n", "")
 
-					td5 := trs.Eq(5).Find("td")
-					level.Point = strings.ReplaceAll(td5.Eq(0).Text(), "\n", "")
-					level.MoveSpeed = strings.ReplaceAll(td5.Eq(1).Text(), "\n", "")
-					level.Weight = strings.ReplaceAll(td5.Eq(2).Text(), "\n", "")
-					level.ElementRes = strings.ReplaceAll(td5.Eq(3).Text(), "\n", "")
-					level.DamageRes = strings.ReplaceAll(td5.Eq(4).Text(), "\n", "")
+					td5 := trs.Eq(6).Find("td")
+					level.AttackType = strings.ReplaceAll(td5.Eq(1).Text(), "\n", "")
+					level.Motion = strings.ReplaceAll(td5.Eq(3).Text(), "\n", "")
 					level.HpRecovery = strings.ReplaceAll(td5.Eq(5).Text(), "\n", "")
+
+					td6 := trs.Eq(6).Find("td")
+					level.HP = strings.ReplaceAll(td6.Eq(0).Text(), "\n", "")
+					level.ATK = strings.ReplaceAll(td6.Eq(1).Text(), "\n", "")
+					level.DEF = strings.ReplaceAll(td6.Eq(2).Text(), "\n", "")
+					level.Res = strings.ReplaceAll(td6.Eq(3).Text(), "\n", "")
+					level.ATKRadius = strings.ReplaceAll(td6.Eq(4).Text(), "\n", "")
+					level.Weight = strings.ReplaceAll(td6.Eq(5).Text(), "\n", "")
+
+					td8 := trs.Eq(8).Find("td")
+					level.MoveSpeed = strings.ReplaceAll(td8.Eq(0).Text(), "\n", "")
+					level.Interval = strings.ReplaceAll(td8.Eq(1).Text(), "\n", "")
+					level.DamageRes = strings.ReplaceAll(td8.Eq(2).Text(), "\n", "")
+					level.ElementRes = strings.ReplaceAll(td8.Eq(3).Text(), "\n", "")
+					level.Ridicule = strings.ReplaceAll(td8.Eq(4).Text(), "\n", "")
+					level.Point = strings.ReplaceAll(td8.Eq(5).Text(), "\n", "")
+
+					td9 := trs.Eq(9).Find("td")
+					level.Abnormal = strings.ReplaceAll(td9.Eq(0).Text(), "\n", "")
 				}
 				if j == 1 {
-					tds := selection.Find("tr").Eq(2).Find("td")
-					level.Silence = strings.ReplaceAll(tds.Eq(0).Text(), "\n", "")
-					level.Dizziness = strings.ReplaceAll(tds.Eq(1).Text(), "\n", "")
-					level.Sleep = strings.ReplaceAll(tds.Eq(2).Text(), "\n", "")
-					level.Freeze = strings.ReplaceAll(tds.Eq(3).Text(), "\n", "")
-					level.Float = strings.ReplaceAll(tds.Eq(4).Text(), "\n", "")
-					level.Tremble = strings.ReplaceAll(tds.Eq(5).Text(), "\n", "")
-					level.Fear = strings.ReplaceAll(tds.Eq(6).Text(), "\n", "")
-				}
-				if j == 2 {
 					var skills []Skill
 					selection.Find("tr").Each(func(k int, selection *goquery.Selection) {
 						if !selection.Children().First().Is("th") {
 							tds := selection.Find("td")
 							// 技能
-							if len(tds.Nodes) == 5 {
+							if len(tds.Nodes) == 4 {
 								var skill Skill
 								skill.Name = strings.ReplaceAll(tds.Eq(0).Text(), "\n", "")
-								skill.SpInit = strings.ReplaceAll(tds.Eq(1).Text(), "\n", "")
-								skill.SpCost = strings.ReplaceAll(tds.Eq(2).Text(), "\n", "")
-								skillType, _ := tds.Eq(3).Html()
-								skill.SkillType = template.HTML(skillType)
-								desc, _ := tds.Eq(4).Html()
+								spInit, _ := tds.Eq(1).Html()
+								skill.SpInit = template.HTML(spInit)
+								spCost, _ := tds.Eq(2).Html()
+								skill.SpCost = template.HTML(spCost)
+								desc, _ := tds.Eq(3).Html()
 								skill.Desc = template.HTML(desc)
 								skills = append(skills, skill)
 							}
